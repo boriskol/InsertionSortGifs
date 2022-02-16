397da4592300dc21eb7e275e230d46431580fb34
borna libertines <2532576+boriskol@users.noreply.github.com>
2022-02-16 10:34:00 -0600

    Initial Commit

---
+++ Shared/InsertionSortGifsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsertionSortGifsApp.swift
+//  Shared
+//
+//  Created by Borna Libertines on 16/02/22.
+//
+
+import SwiftUI
+
+@main
+struct InsertionSortGifsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
